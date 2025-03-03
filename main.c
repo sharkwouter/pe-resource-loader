4891c4fb6283ac69842ba4fc7a4c5ede38d6dd06
Wouter Wijsman <wwijsman@live.nl>
2025-03-03 23:05:05 +0100

    Create initial library functions

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <assert.h>
 
+
+// These are used in open
 #define MZ 0x5A4D
 
 #define AMD64 0x8664
@@ -11,12 +13,14 @@
 
 #define PE32 0x10B
 #define PE32PLUS 0x20B
+// End these are used in open
 
+// These could be used in open
 #define EXPECTED_NT_SIGNATURE "PE"
+#define NT_SIGNATURE_SIZE 4
+// End these could be used in open
 
 #define DIRECTORY_ENTRY_RESOURCE 2
-
-
 
 #define SHORT_NAME_SIZE 8
 
@@ -54,12 +58,11 @@
 #define OPTIONAL_HEADER_64_SIZE 112
 #define DATA_DIRECTORY_SIZE 8
 #define SECTION_HEADER_SIZE 40
-#define NT_SIGNATURE_SIZE 4
 #define RESOURCE_DIRECTORY_TABLE_SIZE 16
 #define RESOURCE_DIRECTORY_ENTRY_SIZE 8
 
 typedef struct __attribute__((packed)) {
-  uint16_t  magic;
+  uint8_t  magic[2];
   uint8_t   unused[58];
   int32_t   nt_header_offset;
 } DosHeader;
@@ -120,11 +123,14 @@ typedef struct __attribute__((packed)) {
   uint32_t  reserved;
 } ResourceDataEntry;
 
-uint32_t virtual_offset = 0;
+typedef struct {
+  FILE *  fd;
+  uint32_t resource_virtual_address;
+  uint32_t resource_offset;
+} PeResourceLoader;
+
 uint32_t section_virtual_address = 0;
 uint32_t section_address = 0;
-uint32_t section_size = 0;
-uint16_t section_alignment = 0;
 
 uint32_t wide_string_length_with_size(uint16_t * str, uint32_t size) {
   size_t full_length = size/sizeof(uint16_t);
@@ -174,43 +180,6 @@ uint8_t * read_directory_name(FILE * fd, uint32_t resource_offset, uint32_t name
   free(name_wide);
 
   return name;
-}
-
-uint8_t * read_string2(FILE * fd, uint32_t resource_offset, uint32_t string_offset, uint32_t size) {
-  uint32_t offset = resource_offset + (string_offset & 0x7FFFFFFF);
-
-
-  fseek(fd, offset, SEEK_SET);
-  uint16_t * name_wide = (uint16_t *) calloc(size, 1);
-  if (NULL == name_wide) {
-    return NULL;
-  }
-  fread(name_wide, 1, size, fd);
-
-  uint32_t length = wide_string_length_with_size(name_wide, size);
-  uint8_t * name = wide_string_to_utf8(name_wide, length);
-  free(name_wide);
-
-  return name;
-}
-
-void read_string(FILE * fd, uint32_t resource_offset, uint32_t data_offset, uint32_t length) {
-  uint32_t offset = resource_offset + (data_offset & 0x7FFFFFFF);
-  uint32_t real_length = length;
-
-  fseek(fd, offset, SEEK_SET);
-  fread(&real_length, sizeof(uint16_t), 1, fd);
-  uint16_t * string = (uint16_t *) calloc(sizeof(uint16_t), real_length);
-  fread(string, sizeof(uint16_t), real_length, fd);
-
-  uint8_t * string_readable = (uint8_t *) calloc(sizeof(uint8_t), real_length);
-  for (int i = 0; i < real_length; i++) {
-    string_readable[i] = (uint8_t) (string[i] & 0x0000FFFF);
-  }
-  free(string);
-
-  printf("String of length %i: %s\n", real_length, string_readable);
-  free(string_readable);
 }
 
 void read_data_entry(FILE * fd, uint32_t resource_offset, uint32_t entry_offset, uint32_t id) {
@@ -391,112 +360,126 @@ void read_type_directory(FILE * fd, uint32_t resource_offset, uint32_t directory
   }
 }
 
-int main(int argc, char ** argv) {
-  // Make sure the above structs are the same size as they are on 
-  assert(sizeof(DosHeader) == DOS_HEADER_SIZE);
-  assert(sizeof(FileHeader) == FILE_HEADER_SIZE);
-  assert(sizeof(OptionalHeader) == OPTIONAL_HEADER_SIZE);
-  assert(sizeof(OptionalHeader64) == OPTIONAL_HEADER_64_SIZE);
-  assert(sizeof(DataDirectory) == DATA_DIRECTORY_SIZE);
-  assert(sizeof(SectionHeader) == SECTION_HEADER_SIZE);
-  assert(sizeof(ResourceDirectoryTable) == RESOURCE_DIRECTORY_TABLE_SIZE);
-  assert(sizeof(ResourceDirectoryEntry) == RESOURCE_DIRECTORY_ENTRY_SIZE);
+void validate_library() {
+  // If any of these fail, you'll need to rewrite the way this library reads data to get it to work
+  assert(sizeof(DosHeader) == 64);
+  assert(sizeof(FileHeader) == 20);
+  assert(sizeof(OptionalHeader) == 96);
+  assert(sizeof(OptionalHeader64) == 112);
+  assert(sizeof(DataDirectory) == 8);
+  assert(sizeof(SectionHeader) == 40);
+  assert(sizeof(ResourceDirectoryTable) == 16);
+  assert(sizeof(ResourceDirectoryEntry) == 8);
+}
 
+#define MZ 0x5A4D
+
+#define AMD64 0x8664
+#define i386 0x14c
+PeResourceLoader * PeResourceLoader_Open(const char * file_path) {
+  validate_library();  // if this fails, we cannot map the content of PE file to our structs
+
+  PeResourceLoader * loader = (PeResourceLoader *) calloc(sizeof(PeResourceLoader), 1);
+  loader->fd = fopen(file_path, "rb");
+  if (loader->fd == NULL) {
+    return NULL;
+  }
+
+  
+  {
+    // Get DOS header and verify it is a DOS header
+    DosHeader dos_header;
+    fread(&dos_header, sizeof(DosHeader), 1, loader->fd);
+    if (strncmp(dos_header.magic, "MZ", 2) != 0) {
+      fclose(loader->fd);
+      free(loader);
+      return NULL;
+    }
+
+    // Verify NT signature
+    uint8_t nt_signature[4];
+    fseek(loader->fd, dos_header.nt_header_offset, SEEK_SET);
+    fread(nt_signature, sizeof(uint8_t), sizeof(nt_signature), loader->fd);
+    if(strcmp(nt_signature, "PE") != 0) {
+      fclose(loader->fd);
+      free(loader);
+      return NULL;
+    }
+  }
+
+  {
+    // Get file header and verify machine type is a supported type
+    FileHeader file_header;
+    fread(&file_header, sizeof(FileHeader), 1, loader->fd);
+    switch (file_header.machine) {
+      case i386:
+        {
+          // Get the number of data directories
+          OptionalHeader optional_header;
+          fread(&optional_header, sizeof(OptionalHeader), 1, loader->fd);
+          if (optional_header.magic != PE32) {
+            fclose(loader->fd);
+            free(loader);
+            return NULL;
+          }
+          // Move cursor to the section header
+          fseek(loader->fd, sizeof(DataDirectory) * optional_header.number_of_data_directories, SEEK_CUR);
+        }
+        break;
+      case AMD64:
+        {
+          // Get the number of data directories
+          OptionalHeader64 optional_header;
+          fread(&optional_header, sizeof(OptionalHeader64), 1, loader->fd);
+          if (optional_header.magic != PE32PLUS) {
+            fclose(loader->fd);
+            free(loader);
+            return NULL;
+          }
+          // Move cursor to the section header
+          fseek(loader->fd, sizeof(DataDirectory) * optional_header.number_of_data_directories, SEEK_CUR);
+        }
+        break;
+      default:
+        fclose(loader->fd);
+        free(loader);
+        return NULL;
+    }
+
+    // Read the section headers
+    SectionHeader * section_headers = (SectionHeader *) calloc(file_header.number_of_sections, sizeof(SectionHeader));
+    fread(section_headers, sizeof(SectionHeader), file_header.number_of_sections, loader->fd);
+    for(int i = 0; i < file_header.number_of_sections; i++) {
+      if (strcmp(".rsrc", section_headers[i].name) == 0) {
+        loader->resource_offset = section_headers[i].address;
+        loader->resource_virtual_address = section_headers[i].virtual_address;
+        break;
+      }
+    }
+    if (loader->resource_offset == 0 || loader->resource_virtual_address == 0) {
+      fclose(loader->fd);
+      free(loader);
+      return NULL;
+    }
+  }
+
+  return loader;
+}
+
+int main(int argc, char ** argv) {
   if (argc != 2) {
     printf("No file was defined\n");
     return 1;
   }
 
-  printf("Opening file %s\n", argv[1]);
-  FILE * fd = fopen(argv[1], "rb");
-  if (fd == NULL) {
-    printf("Could not read file %s\n", argv[1]);
-    return 2;
-  }
+  PeResourceLoader  * loader = PeResourceLoader_Open(argv[1]);
 
-  // Get NT header offset from DOS header
-  DosHeader dos_header;
-  fread(&dos_header, sizeof(DosHeader), 1, fd);
-  assert(dos_header.magic == MZ); // Make sure the magic number in the DOS header is set
-  assert(fseek(fd, dos_header.nt_header_offset, SEEK_SET) == 0); // Navigate to the NT header offset
+  section_address = loader->resource_offset;
+  section_virtual_address = loader->resource_virtual_address;
 
-  // Make sure the NT signature is valid
-  uint8_t * nt_signature = (uint8_t *) calloc(1, NT_SIGNATURE_SIZE);
-  fread(nt_signature, 1, NT_SIGNATURE_SIZE, fd);
-  assert(strcmp(nt_signature, EXPECTED_NT_SIGNATURE) == 0); // Make sure this is the NT header
-  free(nt_signature);
-
-  // Get the machine type from the file header
-  FileHeader * file_header = (FileHeader *) calloc(1, sizeof(FileHeader));
-  fread(file_header, sizeof(FileHeader), 1, fd);
-
-  DataDirectory * data_directories = NULL;
-  uint16_t file_alignment = 0;
-  uint32_t number_of_data_directories = 0;
-  switch (file_header->machine) {
-    case i386:
-      {
-        // Get the number of data directories
-        OptionalHeader optional_header;
-        fread(&optional_header, sizeof(OptionalHeader), 1, fd);
-        assert(optional_header.magic == PE32); // If this fails we're either dealing with ROM or invalid data
-        //printf("NT Magic: %s\n", optional_header.magic);
-        section_alignment = optional_header.section_alignment;
-        number_of_data_directories = optional_header.number_of_data_directories;
-        printf("Section alignment and file alignment: %i, %i\n", section_alignment, optional_header.file_alignment);
-
-        // Get the data directory list to get the resource directory location
-        data_directories = (DataDirectory *) calloc(optional_header.number_of_data_directories, sizeof(DataDirectory));
-        fread(data_directories, sizeof(DataDirectory), optional_header.number_of_data_directories, fd);
-      }
-      break;
-    case AMD64:
-      {
-        // Get the number of data directories
-        OptionalHeader64 optional_header;
-        fread(&optional_header, sizeof(OptionalHeader64), 1, fd);
-        assert(optional_header.magic == PE32PLUS); // If this fails we're either dealing with ROM or invalid data
-        section_alignment = optional_header.section_alignment;
-        number_of_data_directories = optional_header.number_of_data_directories;
-        printf("Section alignment and file alignment: %i, %i\n", section_alignment, optional_header.file_alignment);
-  
-        // Get the data directory list to get the resource directory location
-        data_directories = (DataDirectory *) calloc(optional_header.number_of_data_directories, sizeof(DataDirectory));
-        fread(data_directories, sizeof(DataDirectory), optional_header.number_of_data_directories, fd);
-      }
-      break;
-    default:
-      printf("Unsupported machine type\n");
-      return 3;
-      break;
-  }
- 
-  printf("Number of directories:%i\n", number_of_data_directories);
-  if (number_of_data_directories > DIRECTORY_ENTRY_RESOURCE && data_directories[DIRECTORY_ENTRY_RESOURCE].offset == 0 || data_directories[DIRECTORY_ENTRY_RESOURCE].size == 0) {
-    printf("No resources found in file %s\n", argv[1]);
-    return 4;
-  }
-  free(data_directories);
-
-  // Read the section headers
-  uint32_t resource_offset;
-  SectionHeader * section_headers = (SectionHeader *) calloc(file_header->number_of_sections, sizeof(SectionHeader));
-  fread(section_headers, sizeof(SectionHeader), file_header->number_of_sections, fd);
-  for(int i = 0; i < file_header->number_of_sections; i++) {
-    if (strcmp(".rsrc", section_headers[i].name) == 0) {
-      printf("Found .rsrc\n");
-      resource_offset = section_headers[i].address;
-      printf("Address: real=%i, virtual=%i\n", section_headers[i].address, section_headers[i].virtual_address);
-      virtual_offset = section_headers[i].virtual_address - section_headers[i].address;
-      printf("Virtual offset is %i\n", virtual_offset);
-      section_virtual_address = section_headers[i].virtual_address;
-      section_address = section_headers[i].address;
-    }
-  }
-
-  read_type_directory(fd, resource_offset, 0);
-  fclose(fd);
-  free(file_header);
+  read_type_directory(loader->fd, loader->resource_offset, 0);
+  fclose(loader->fd);
+  free(loader);
 
   return 0; 
 }
