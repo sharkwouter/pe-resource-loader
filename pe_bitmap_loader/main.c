806a8d549fedf43c692d56a25b36db8910cb7e58
Wouter Wijsman <wouter@gridscale.io>
2025-08-20 14:21:23 +0200

    Make loader examples able to handle multiple files as input

--- pe_bitmap_loader/main.c
+++ pe_bitmap_loader/main.c
@@ -7,56 +7,70 @@
 #include "stb_image_write.h"
 
 int main(int argc, char ** argv) {
-  if (argc != 2) {
+  if (argc < 2) {
     printf("No file was defined\n");
     return 1;
   }
 
-  PeResourceLoader  * loader = PeResourceLoader_Open(argv[1]);
-  if (!loader) {
-    printf("File %s does not exist or is not a valid PE file\n", argv[1]);
-    return 2;
-  }
+  int return_value = 0;
+  for (int i = 1; i < argc; i++) {
+    printf("Bitmaps from file %s:\n", argv[i]);
+    PeResourceLoader  * loader = PeResourceLoader_Open(argv[i]);
+    if (!loader) {
+      printf("File %s does not exist or is not a valid PE file\n", argv[i]);
+      return_value = 2;
+      continue;
+    }
 
-  uint32_t bitmap_count = 0;
-  uint32_t * bitmap_ids = PeResourceLoader_GetResourceIds(loader, PRL_TYPE_BITMAP, &bitmap_count);
-  if (bitmap_count == 0) {
-    printf("No bitmaps found in file %s\n", argv[1]);
+    uint32_t bitmap_count = 0;
+    uint32_t * bitmap_ids = PeResourceLoader_GetResourceIds(loader, PRL_TYPE_BITMAP, &bitmap_count);
+    if (bitmap_count == 0) {
+      printf("No bitmaps found in file %s\n", argv[i]);
+      PeResourceLoader_Close(loader);
+      if (bitmap_ids) {
+        free(bitmap_ids);
+      }
+      return_value = 3;
+      continue;
+    }
+
+    uint16_t language_count = 0;
+    uint32_t * languages = PeResourceLoader_GetLanguageIds(loader, &language_count);
+    if (language_count == 0) {
+      printf("No languages found in file %s\n", argv[i]);
+      PeResourceLoader_Close(loader);
+      if (bitmap_ids) {
+        free(bitmap_ids);
+      }
+      if (languages) {
+        free(languages);
+      }
+      return_value = 4;
+      continue;
+    }
+
+    for (uint16_t li = 0; li < language_count; li++) {
+      printf("Language with id %u:\n", languages[li]);
+      for (uint16_t bi = 0; bi < bitmap_count; bi++) {
+        printf("Found bitmap with id %u\n", bitmap_ids[bi]);
+        char * file_name = calloc(sizeof(char), 30);
+        snprintf(file_name, 30, "%u_%u.bmp\0", languages[li], bitmap_ids[bi]);
+        FILE * file = fopen(file_name, "wb");
+        free(file_name);
+        uint32_t file_size;
+        void * data = PeResourceLoader_GetResource(loader, PRL_TYPE_BITMAP, languages[li], bitmap_ids[bi], &file_size);
+        printf("File size is %u\n", file_size);
+        fwrite(data, 1, file_size, file);
+        fclose(file);
+        free(data);
+      }
+      printf("\n");
+    }
+    free(languages);
+    free(bitmap_ids);
+
     PeResourceLoader_Close(loader);
-    if (bitmap_ids) {
-      free(bitmap_ids);
-    }
-    return 3;
   }
 
-  uint16_t language_count = 0;
-  uint32_t * languages = PeResourceLoader_GetLanguageIds(loader, &language_count);
-  if (language_count == 0) {
-    printf("No languages found in file %s\n", argv[1]);
-    return 4;
-  }
-
-  for (uint16_t li = 0; li < language_count; li++) {
-    printf("Language with id %u:\n", languages[li]);
-    for (uint16_t bi = 0; bi < bitmap_count; bi++) {
-      printf("Found bitmap with id %u\n", bitmap_ids[bi]);
-      char * file_name = calloc(sizeof(char), 30);
-      snprintf(file_name, 30, "%u_%u.bmp\0", languages[li], bitmap_ids[bi]);
-      FILE * file = fopen(file_name, "wb");
-      free(file_name);
-      uint32_t file_size;
-      void * data = PeResourceLoader_GetResource(loader, PRL_TYPE_BITMAP, languages[li], bitmap_ids[bi], &file_size);
-      printf("File size is %u\n", file_size);
-      fwrite(data, 1, file_size, file);
-      fclose(file);
-      free(data);
-    }
-    printf("\n");
-  }
-  free(languages);
-  free(bitmap_ids);
-
-  PeResourceLoader_Close(loader);
-
-  return 0; 
+  return return_value;
 }
--- pe_string_loader/main.c
+++ pe_string_loader/main.c
@@ -4,47 +4,65 @@
 #include <pe_resource_loader.h>
 
 int main(int argc, char ** argv) {
-  if (argc != 2) {
+  if (argc < 2) {
     printf("No file was defined\n");
     return 1;
   }
 
-  PeResourceLoader  * loader = PeResourceLoader_Open(argv[1]);
-  if (!loader) {
-    printf("File %s does not exist or is not a valid PE file\n", argv[1]);
-    return 2;
-  }
-
-  uint32_t string_count = 0;
-  uint32_t * string_ids = PeResourceLoader_GetResourceIds(loader, PRL_TYPE_STRING, &string_count);
-  if (string_count == 0) {
-    printf("No strings found in file %s\n", argv[1]);
-    return 3;
-  }
-
-  uint16_t language_count = 0;
-  uint32_t * languages = PeResourceLoader_GetLanguageIds(loader, &language_count);
-  if (language_count == 0) {
-    printf("No languages found in file %s\n", argv[1]);
-    return 4;
-  }
-
-  for (uint16_t li = 0; li < language_count; li++) {
-    printf("Strings for language with id %u:\n", languages[li]);
-    for (uint16_t si = 0; si < string_count; si++) {
-      uint32_t length = 0;
-      char * string = (char *) PeResourceLoader_GetResource(loader, PRL_TYPE_STRING, languages[li], string_ids[si], &length);
-      if (string) {
-        printf("%u: %s\n", string_ids[si], string);
-      }
-      free(string);
+  int return_value = 0;
+  for(int i = 1; i < argc; i++) {
+    printf("String from file %s:\n", argv[i]);
+    PeResourceLoader * loader = PeResourceLoader_Open(argv[i]);
+    if (!loader) {
+      printf("File %s does not exist or is not a valid PE file\n", argv[i]);
+      return_value = 2;
+      continue;
     }
-    printf("\n");
+
+    uint32_t string_count = 0;
+    uint32_t * string_ids = PeResourceLoader_GetResourceIds(loader, PRL_TYPE_STRING, &string_count);
+    if (string_count == 0) {
+      printf("No strings found in file %s\n", argv[i]);
+      PeResourceLoader_Close(loader);
+      if (string_ids) {
+        free(string_ids);
+      }
+      return_value = 3;
+      continue;
+    }
+
+    uint16_t language_count = 0;
+    uint32_t * languages = PeResourceLoader_GetLanguageIds(loader, &language_count);
+    if (language_count == 0) {
+      printf("No languages found in file %s\n", argv[i]);
+      PeResourceLoader_Close(loader);
+      if (string_ids) {
+        free(string_ids);
+      }
+      if (languages) {
+        free(languages);
+      }
+      return_value = 4;
+      continue;
+    }
+
+    for (uint16_t li = 0; li < language_count; li++) {
+      printf("Strings for language with id %u:\n", languages[li]);
+      for (uint16_t si = 0; si < string_count; si++) {
+        uint32_t length = 0;
+        char * string = (char *) PeResourceLoader_GetResource(loader, PRL_TYPE_STRING, languages[li], string_ids[si], &length);
+        if (string) {
+          printf("%u: %s\n", string_ids[si], string);
+        }
+        free(string);
+      }
+      printf("\n");
+    }
+    free(languages);
+    free(string_ids);
+
+    PeResourceLoader_Close(loader);
   }
-  free(languages);
-  free(string_ids);
 
-  PeResourceLoader_Close(loader);
-
-  return 0; 
+  return return_value;
 }
